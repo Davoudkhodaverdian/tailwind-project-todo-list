037b1d7bc1d1ca9f949210c184d519a8d2e744ed
Davoudkhodaverdian <davoudkhodaverdian20@gmail.com>
2022-05-27 17:14:12 +0430

    add tailwind

---
+++ src/homePage/homePage.js
@@ -0,0 +1,14 @@
+
+import './homePage.css';
+
+function homePage() {
+    return (
+        <div >
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </div>
+    );
+}
+
+export default homePage;
