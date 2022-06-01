fa61833c27121dcf429c17b68ce553bd0343bf8f
Davoudkhodaverdian <davoudkhodaverdian20@gmail.com>
2022-06-01 14:24:40 +0430

    modify index.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import HomePage from './components/homePage/homePage';
- import PricingSections from './components/pricingSections/pricingSections';
 import reportWebVitals from './reportWebVitals';
 import TodoList from './components/todoList/todoList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <HomePage /> */}
-    <PricingSections />
+    <HomePage />
     {/* <TodoList /> */}
   </React.StrictMode>
 );
