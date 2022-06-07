3ade6e02e88d0b621588ed23978c5e629711318d
Davoudkhodaverdian <davoudkhodaverdian20@gmail.com>
2022-06-07 12:47:53 +0430

    remove user list directory

--- src/components/homePage/navBar/navBar.js
+++ src/components/homePage/navBar/navBar.js
@@ -33,7 +33,7 @@ function NavBar() {
                 >
 
                     <a href="#" className="px-3 rounded-lg text-white text-center bg-violet-500 font-bold drop-shadow hover:bg-violet-600 active:bg-violet-700 focus:ring focus:ring-violet-300 transition-all">todo list</a>
-                    <a href="#" className="px-3 rounded-lg text-white text-center bg-violet-500 font-bold drop-shadow hover:bg-violet-600 active:bg-violet-700 focus:ring focus:ring-violet-300 transition-all">user list</a>
+
 
                 </div>
 
