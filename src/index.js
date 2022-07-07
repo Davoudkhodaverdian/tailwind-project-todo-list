6843fba2fa6e1aade521470b7861932c1b5ee369
Davoudkhodaverdian <davoudkhodaverdian20@gmail.com>
2022-07-08 02:06:56 +0430

    add redux persist

--- src/index.js
+++ src/index.js
@@ -4,21 +4,20 @@ import './index.css';
 import Main from './components/main/main';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { store } from './store/store';
-// import { store, persistor } from './store/store';
-// import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react'
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
+      <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
 
           <Main />
         </BrowserRouter>
-      {/* </PersistGate> */}
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -3,9 +3,9 @@ import todoSlice from './slices/todoSlice'
 import userListSlice from './slices/userListSlice'
 import counterSlice from './slices/counterSlice'
 import authenticateSlice from './slices/authenticateSlice'
-
-//import { persistStore, persistReducer } from 'redux-persist'
-//import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { getDefaultMiddleware } from '@reduxjs/toolkit';
 
 const rootReducer = combineReducers({
   todo: todoSlice,
@@ -16,19 +16,25 @@ const rootReducer = combineReducers({
 
 
 //without persist
-const store = configureStore({ reducer: rootReducer })
-export { store }
+// const store = configureStore({ reducer: rootReducer })
+// export { store }
 
 
 //with persist
-// const persistConfig = {
-//   key: 'root',
-//   storage,
-//   whitelist: [] // only this item will be persisted
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ["authenticate"] // only this item will be persisted
+}
 
-// }
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer)
-// const store = configureStore({ reducer: persistedReducer })
-// const persistor = persistStore(store)
-// export { store, persistor }
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+const store = configureStore({
+  reducer: persistedReducer,
+  //non-serializable value
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+})
+const persistor = persistStore(store)
+export { store, persistor }
